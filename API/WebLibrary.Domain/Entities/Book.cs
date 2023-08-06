c3b5481c1b6e18bf73943c21307cd617aac7fc43
Vladislav <vsoilol1701@gmail.com>
2023-08-06 16:02:30 +0300

    feat: add entities classes

---
+++ API/WebLibrary.Domain/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebLibrary.Domain.Entities;
+
+[Table(nameof(Book))]
+public class Book
+{
+    public string Isbn { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string Genre { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+
+    public DateTime BorrowedTime { get; set; }
+
+    public DateTime ReturnDueTime { get; set; }
+}
---
+++ API/WebLibrary.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebLibrary.Domain.Entities;
+
+[Table(nameof(User))]
+public class User : Entity
+{
+    public string FirstName { get; set; } = null!;
+    
+    public string LastName { get; set; } = null!;
+    
+    public string MiddleName { get; set; } = null!;
+
+    public string Login { get; set; } = null!;
+
+    public string? PasswordHash { get; set; }
+}
