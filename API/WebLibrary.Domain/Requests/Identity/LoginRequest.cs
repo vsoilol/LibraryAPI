ac93788457e3316028ee8661ab886b2a4181902e
Vladislav <vsoilol1701@gmail.com>
2023-08-07 16:33:50 +0300

    feat: add identity requests

---
+++ API/WebLibrary.Domain/Requests/Identity/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace WebLibrary.Domain.Requests.Identity;
+
+public class LoginRequest
+{
+    public string Login { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
---
+++ API/WebLibrary.Domain/Requests/Identity/RegisterRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace WebLibrary.Domain.Requests.Identity;
+
+public class RegisterRequest
+{
+    public string FirstName { get; set; } = null!;
+    
+    public string LastName { get; set; } = null!;
+    
+    public string MiddleName { get; set; } = null!;
+
+    public string Login { get; set; } = null!;
+    
+    public string Password { get; set; } = null!;
+}
