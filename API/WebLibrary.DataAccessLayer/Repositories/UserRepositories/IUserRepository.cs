7e8e268d9264aac5f1fb84e68f135265878032d4
Vladislav <vsoilol1701@gmail.com>
2023-08-07 16:18:59 +0300

    feat: add user repository

---
+++ API/WebLibrary.DataAccessLayer/Repositories/UserRepositories/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using WebLibrary.Domain.Entities;
+
+namespace WebLibrary.DataAccessLayer.Repositories.UserRepositories;
+
+public interface IUserRepository : IRepository<User>
+{
+    Task<User?> GetUserByLoginAsync(string login);
+}
---
+++ API/WebLibrary.DataAccessLayer/Repositories/UserRepositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebLibrary.DataAccessLayer.DataAccess;
+using WebLibrary.Domain.Entities;
+
+namespace WebLibrary.DataAccessLayer.Repositories.UserRepositories;
+
+internal class UserRepository : GenericRepository<User>, IUserRepository
+{
+    public UserRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public Task<User?> GetUserByLoginAsync(string login)
+    {
+        return DbSet.AsNoTracking().FirstOrDefaultAsync(_ => _.Login == login);
+    }
+}
