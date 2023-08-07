49bf47fcbde15665321677ecd429a1dce26dd9f1
Vladislav <vsoilol1701@gmail.com>
2023-08-07 16:15:08 +0300

    feat: add JWT authentication with authorization

---
+++ API/WebLibrary.API/Configurations/AuthenticationConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using WebLibrary.Domain.Settings;
+
+namespace WebLibrary.API.Configurations;
+
+public static class AuthenticationConfiguration
+{
+    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var authConfigSection = configuration.GetSection("Auth");
+        
+        var authSettings = new AuthSettings();
+        configuration.Bind(nameof(authSettings), authSettings);
+        services.AddSingleton(authSettings);
+
+        services.Configure<AuthSettings>(authConfigSection);
+        
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authSettings.Secret)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true
+                };
+            });
+        services.AddAuthorization();
+        
+        return services;
+    }
+}
--- API/WebLibrary.API/Program.cs
+++ API/WebLibrary.API/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
 var app = builder.Build();
 
@@ -16,6 +17,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
