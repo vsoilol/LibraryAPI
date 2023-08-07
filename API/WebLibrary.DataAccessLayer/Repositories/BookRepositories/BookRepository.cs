32f33e9ed35d36e54404cd49cb4fea3e0bd98c92
Vladislav <vsoilol1701@gmail.com>
2023-08-07 16:19:10 +0300

    feat: add book repository

---
+++ API/WebLibrary.DataAccessLayer/Repositories/BookRepositories/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebLibrary.DataAccessLayer.DataAccess;
+using WebLibrary.Domain.Entities;
+
+namespace WebLibrary.DataAccessLayer.Repositories.BookRepositories;
+
+internal class BookRepository : GenericRepository<Book>, IBookRepository
+{
+    public BookRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public Task<Book?> GetByIsbnAsync(string isbn)
+    {
+        return DbSet.AsNoTracking().FirstOrDefaultAsync(_ => _.Isbn == isbn);
+    }
+}
---
+++ API/WebLibrary.DataAccessLayer/Repositories/BookRepositories/IBookRepository.cs
@@ -0,0 +1,8 @@
+﻿using WebLibrary.Domain.Entities;
+
+namespace WebLibrary.DataAccessLayer.Repositories.BookRepositories;
+
+public interface IBookRepository : IRepository<Book>
+{
+    Task<Book?> GetByIsbnAsync(string isbn);
+}
