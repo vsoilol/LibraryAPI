8f71cfe5a6d1d3642336088060a9c33423a8bb7e
Vladislav <vsoilol1701@gmail.com>
2023-08-07 16:15:53 +0300

    feat: add swagger configuration

---
+++ API/WebLibrary.API/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace WebLibrary.API.Configurations;
+
+public static class SwaggerConfiguration
+{
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(x =>
+        {
+            x.SwaggerDoc("v1", new OpenApiInfo { Title = "Library API", Version = "v1" });
+
+            var securityScheme = new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JSON Web Token based security",
+            };
+
+            var securityReq = new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            };
+
+            x.AddSecurityDefinition("Bearer", securityScheme);
+            x.AddSecurityRequirement(securityReq);
+        });
+        
+        return services;
+    }
+}
--- API/WebLibrary.API/Program.cs
+++ API/WebLibrary.API/Program.cs
@@ -3,9 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwagger();
 builder.Services.AddJwtAuthentication(builder.Configuration);
 
 var app = builder.Build();
