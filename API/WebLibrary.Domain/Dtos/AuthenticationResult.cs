aee25c7d9a6ba33cde3b6d4754b6bf3b1c54d612
Vladislav <vsoilol1701@gmail.com>
2023-08-07 16:34:01 +0300

    feat: add AuthenticationResult

---
+++ API/WebLibrary.Domain/Dtos/AuthenticationResult.cs
@@ -0,0 +1,21 @@
+﻿namespace WebLibrary.Domain.Dtos;
+
+public class AuthenticationResult
+{
+    public string? Token { get; set; }
+
+    public bool Success { get; set; }
+
+    public IEnumerable<string>? Errors { get; set; }
+
+    public AuthenticationResult(IEnumerable<string> errors)
+    {
+        Errors = errors;
+    }
+
+    public AuthenticationResult(string token)
+    {
+        Success = true;
+        Token = token;
+    }
+}
