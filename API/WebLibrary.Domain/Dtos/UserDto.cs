3081527b62cbebb4aeedbd6d74c1d4b1cc86d067
Vladislav <vsoilol1701@gmail.com>
2023-08-07 16:22:57 +0300

    feat: add user and book dtos

---
+++ API/WebLibrary.Domain/Dtos/UserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace WebLibrary.Domain.Dtos;
+
+public class UserDto
+{
+    public Guid Id { get; set; }
+    
+    public string FirstName { get; set; } = null!;
+    
+    public string LastName { get; set; } = null!;
+    
+    public string MiddleName { get; set; } = null!;
+
+    public string Login { get; set; } = null!;
+}
