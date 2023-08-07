1ed46cbd1788875f52c82ff9e355c50ab4fa2845
Vladislav <vsoilol1701@gmail.com>
2023-08-07 16:26:36 +0300

    feat: add mapping configs for book and user

---
+++ API/WebLibrary.BusinessLayer/Mappings/BookMappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using Mapster;
+using WebLibrary.Domain.Dtos;
+using WebLibrary.Domain.Entities;
+using WebLibrary.Domain.Requests.Book;
+
+namespace WebLibrary.BusinessLayer.Mappings;
+
+internal class BookMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<Book, BookDto>()
+            .RequireDestinationMemberSource(true);
+
+        config.NewConfig<CreateBookRequest, Book>()
+            .RequireDestinationMemberSource(true)
+            .Ignore(dest => dest.Id);
+        
+        config.NewConfig<UpdateBookRequest, Book>()
+            .RequireDestinationMemberSource(true);
+    }
+}
---
+++ API/WebLibrary.BusinessLayer/Mappings/UserMappingConfig.cs
@@ -0,0 +1,26 @@
+﻿using Mapster;
+using WebLibrary.Domain.Dtos;
+using WebLibrary.Domain.Entities;
+using WebLibrary.Domain.Requests.Identity;
+using WebLibrary.Domain.Requests.User;
+
+namespace WebLibrary.BusinessLayer.Mappings;
+
+internal class UserMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<User, UserDto>()
+            .RequireDestinationMemberSource(true);
+
+        config.NewConfig<RegisterRequest, User>()
+            .RequireDestinationMemberSource(true)
+            .Ignore(dest => dest.Id)
+            .Ignore(dest => dest.PasswordHash);
+
+        config.NewConfig<UpdateUserRequest, User>()
+            .RequireDestinationMemberSource(true)
+            .Ignore(dest => dest.Login)
+            .Ignore(dest => dest.PasswordHash);
+    }
+}
