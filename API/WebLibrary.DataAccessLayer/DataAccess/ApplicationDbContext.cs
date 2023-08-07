24d17c08510b7a2b5bfc76106c301d20b5ac5f21
Vladislav <vsoilol1701@gmail.com>
2023-08-07 16:18:12 +0300

    feat: add ApplicationDbContext

---
+++ API/WebLibrary.DataAccessLayer/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebLibrary.Domain.Entities;
+
+namespace WebLibrary.DataAccessLayer.DataAccess;
+
+internal sealed class ApplicationDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    public DbSet<Book> Books { get; set; } = null!;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
