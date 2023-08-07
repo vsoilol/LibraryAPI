2d2be5fc7e4b32c5bafd9566911192e39bbc1cd2
Vladislav <vsoilol1701@gmail.com>
2023-08-07 16:31:53 +0300

    feat: add validation for CreateBookRequest and UpdateBookRequest

---
+++ API/WebLibrary.BusinessLayer/Validation/Validators/BookValidators/CreateBookRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using WebLibrary.BusinessLayer.Validation.Constants;
+using WebLibrary.BusinessLayer.Validation.Models;
+using WebLibrary.DataAccessLayer.Repositories.BookRepositories;
+using WebLibrary.Domain.Requests.Book;
+
+namespace WebLibrary.BusinessLayer.Validation.Validators.BookValidators;
+
+internal class CreateBookRequestValidator : IValidator<CreateBookRequest>
+{
+    private readonly IBookRepository _bookRepository;
+
+    public CreateBookRequestValidator(IBookRepository bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+
+    public async Task<ValidationResult> IsInstanceValidAsync(CreateBookRequest instance)
+    {
+        var existingBook = await _bookRepository.GetByIsbnAsync(instance.Isbn);
+
+        if (existingBook is null)
+        {
+            return ValidationResult.Success;
+        }
+
+        var errorMessage = string.Format(ValidationErrorMessages.BookWithIsbnAlreadyExists, instance.Isbn);
+        var validationResult = new ValidationResult(new List<string> { errorMessage });
+        return validationResult;
+    }
+}
---
+++ API/WebLibrary.BusinessLayer/Validation/Validators/BookValidators/UpdateBookRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using WebLibrary.BusinessLayer.Validation.Constants;
+using WebLibrary.BusinessLayer.Validation.Models;
+using WebLibrary.DataAccessLayer.Repositories.BookRepositories;
+using WebLibrary.Domain.Requests.Book;
+
+namespace WebLibrary.BusinessLayer.Validation.Validators.BookValidators;
+
+internal class UpdateBookRequestValidator : IValidator<UpdateBookRequest>
+{
+    private readonly IBookRepository _bookRepository;
+
+    public UpdateBookRequestValidator(IBookRepository bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+
+    public async Task<ValidationResult> IsInstanceValidAsync(UpdateBookRequest instance)
+    {
+        var existingBook = await _bookRepository.GetByIsbnAsync(instance.Isbn);
+
+        if (existingBook is null || existingBook.Id == instance.Id)
+        {
+            return ValidationResult.Success;
+        }
+
+        var errorMessage = string.Format(ValidationErrorMessages.BookWithIsbnAlreadyExists, instance.Isbn);
+        var validationResult = new ValidationResult(new List<string> { errorMessage });
+        return validationResult;
+    }
+}
